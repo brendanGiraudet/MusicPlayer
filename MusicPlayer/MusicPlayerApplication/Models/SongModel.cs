c82ec82269fa3df30a8c8ca5e2663082ff239c62
baka <bakayarusama@gmail.com>
2021-01-09 00:53:15 +0100

    Add jsonpropertyname annotation

--- MusicPlayer/MusicPlayerApplication/Models/SongModel.cs
+++ MusicPlayer/MusicPlayerApplication/Models/SongModel.cs
@@ -1,9 +1,13 @@
-﻿namespace MusicPlayerApplication.Models
+﻿using System.Text.Json.Serialization;
+
+namespace MusicPlayerApplication.Models
 {
     public class SongModel
     {
+        [JsonPropertyName("title")]
         public string Title { get; set; }
         public string Path { get; set; }
+        [JsonPropertyName("artist")]
         public string Artist { get; set; }
         public string ImagePath { get; set; }
     }
