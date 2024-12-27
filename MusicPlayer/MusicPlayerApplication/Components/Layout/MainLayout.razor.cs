59a2055c23a49f891c23ca35b7190fad6c5346ce
baka <brendan.giraudet1@gmail.com>
2024-12-27 16:51:20 +0100

    add version into footer

--- MusicPlayer/MusicPlayerApplication/Components/Layout/MainLayout.razor.cs
+++ MusicPlayer/MusicPlayerApplication/Components/Layout/MainLayout.razor.cs
@@ -1,6 +1,10 @@
-﻿namespace MusicPlayerApplication.Components.Layout;
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Configuration;
+
+namespace MusicPlayerApplication.Components.Layout;
 
 public partial class MainLayout
 {
+    [Inject]public IConfiguration Configuration { get; set; }
     private bool sidebarExpanded = false;
 }
